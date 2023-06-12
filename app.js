11b8198d5adb06384e2d85aabdca52750a9c145d
kosimovdev <kosimov.oyatillo@gmail.com>
2023-06-12 08:10:17 +0500

    done

---
+++ app.js
@@ -0,0 +1,117 @@
+"use strick"
+
+// 1chi masala
+
+// let num = 5;
+
+// const daraja =function(num){
+//     return num**3
+// }
+// console.log(daraja(num));
+
+// 2chi masala
+
+// let num1 = +prompt("num1");
+// let num2 = +prompt("num2");
+
+// const yigindi = function(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(yigindi(num1, num2));
+
+// 3chi masala
+
+// let num1 = +prompt("num1");
+// let num2 = +prompt("num2");
+
+// const yigindi = function(num1, num2){
+//     return num1 - num2;
+// }
+
+// console.log(yigindi(num1, num2));
+
+// 4chi masala
+
+// let num = +prompt("num1");
+
+// const darajalar = function(num1){
+//    console.log(nimadur , ${num**2} ${num**3} ${num**4});
+// }
+
+// darajalar(num)
+
+// 5chi masala
+
+// let num = +prompt("num");
+
+// const even = function(num){
+//     for (let i=0; i<num; i++){
+//         if(i %2 == 0){
+//         console.log(i);
+//         }
+//     }
+// }
+
+// even(num)
+
+// 6chi masala
+
+// let num = +prompt("num");
+// let num = 10;
+
+// const notEven = function(num){
+//     for (let i=0; i<num; i++){
+//         if(i %2 !== 0){
+//         console.log(i);
+//         }
+//     }
+// }
+
+// notEven(num)
+
+// 8chi masala
+// let num =10
+// const buluv = function(num){
+//     for(let i=0; i<=num;i++){
+//         if(num %i == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// buluv(num)
+
+// 9chi masala
+// let num1 = 5
+// let num2 = 3
+// let belgi ="*"
+
+// const amal =function(num){
+//     if(belgi === "+"){
+//         return num1 + num2
+//     }else if(belgi ==="-"){
+//         return num1 - num2
+//     }else if(belgi === "*"){
+//         return num1 * num2
+//     }else if(belgi === "/"){
+//         return num1 / num2
+//     }
+
+// }
+
+// console.log(amal(num1,num2));
+
+// 10chi masala
+// let num = 5
+// const karra = function (num) {
+//   for (let i = 2; i <= 5; i++) {
+//     let show = 0;
+//     for (let j = 1; j <= 10; j++) {
+//       show = i * j;
+//       console.log(${i} * ${j} = ${show});
+//     }
+//     console.log("\n\n");
+//   }
+// };
+// karra(num)
